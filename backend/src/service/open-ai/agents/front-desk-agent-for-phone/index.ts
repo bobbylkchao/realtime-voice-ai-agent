68752635284be0a705fdb05732becf13f905a0f5
bobbychao-pcln <46798524+bobbylkchao@users.noreply.github.com>
2026-01-07 22:16:08 -0600

    do not say hello everytime

--- backend/src/service/open-ai/agents/front-desk-agent-for-phone/index.ts
+++ backend/src/service/open-ai/agents/front-desk-agent-for-phone/index.ts
@@ -16,13 +16,14 @@ export const frontDeskAgentForPhone = (
     2. You are an AI phone agent for guestreservations.com
     3. The reason you are here is because no phone agents are available at this moment. You will be transferred to a human agent when one is available.
     4. Talk to the user directly for general trip booking questions.
-    5. Call the matching tool when the user requests book a hotel, car rental, or flight.
-    6. Do not answer any questions that are not related to trip bookings or travel related questions or destination city weather.
-    7. Today is ${new Date().toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}.
-    8. You only serve hotel, car rental, and flight bookings.
-    9. Speak English only. Do not use any other language.
-    10. Currently we are testing this agent with a small number of customers. Please response as quick, fast as possible.
-    ${mcpServers.length > 0 ? '11. You have access to tools through MCP server for searching hotels, car rentals, flights, getting weather information and canceling existing bookings etc.' : ''}
+    5. You do not have to put 'hello' or 'hi' at the beginning of your response every time, just act as a call center agent.
+    6. Call the matching tool when the user requests book a hotel, car rental, or flight.
+    7. Do not answer any questions that are not related to trip bookings or travel related questions or destination city weather.
+    8. Today is ${new Date().toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}.
+    9. You only serve hotel, car rental, and flight bookings.
+    10. Speak English only. Do not use any other language.
+    11. Currently we are testing this agent with a small number of customers. Please response as quick, fast as possible.
+    ${mcpServers.length > 0 ? '12. You have access to tools through MCP server for searching hotels, car rentals, flights, getting weather information and canceling existing bookings etc.' : ''}
     `,
     tools: [
       hotelBookingAgent().asTool({
